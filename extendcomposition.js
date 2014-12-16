65d1e9e0d191e74abf4925bffebcb0b98a0fd772
Radahika <radhika.marvin@berkeley.edu>
2014-12-16 14:09:43 -0800

    moved nested test file

---
+++ extendcomposition.js
@@ -0,0 +1,19 @@
+if (typeof(module) !== 'undefined') {
+	var rhs = require('./rhs.js').rhs;
+	var automata = require('./automata.js').automata;
+}
+
+var re = new rhs("start");
+var func0 = re.compose(rhs.alter)(rhs.alter)(rhs.repeat)();
+console.log('\n', func0("abc", "def", [4,8]).toString());
+var nested = rhs.compose(func0)(rhs.concat)(rhs.repeat)(rhs.concat)(rhs.single)(rhs.concat)();
+console.log('\n', nested(["abc", "def", [4, 8]], "end", 10000, "Please", [], ["yeah!"]).toString());
+//(((abc)|(def)){4,8}end){10000}Please/
+
+var re = new rhs("start");
+var func1 = re.compose(rhs.alter)(rhs.repeat)();
+var func2 = re.compose(func1)(rhs.alter)(rhs.repeat)(rhs.concat)();
+console.log('\n',func2(["friend group", [4, 8]], "[^Soham]", 10000, " Dear Bodik, please be merciful on our grades").toString());
+// ((((start)|(friend group)){4,8})|([^Soham])){10000} Dear Bodik, please be merciful on our grades
+
+
