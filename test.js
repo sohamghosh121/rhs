399b163ecdd5dfe259f2324f8acd65928dc52ab4
Radahika <radhika.marvin@berkeley.edu>
2014-12-15 17:47:27 -0800

    Writing email tests

--- test.js
+++ test.js
@@ -3,4 +3,18 @@ if (typeof(module) !== 'undefined') {
 	var automata = require('./automata.js').automata;
 }
 
-//console.log(automata.fromRE("a|b"));
+
+/* "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?" */
+
+var allowedChars = "a-zA-Z0-9"
+var symbols = "!#$%&'*+/=?^_`{|}~-";
+var allowedChars = rhs.group(allowedChars, symbols).repeat(1, Infinity);
+
+var dot = "?:/."
+new rhs(dot)
+var dotCharRepeat = rhs.concat(dot, allowedChars).repeat(0, Infinity);
+email = rhs.concat(allowedChars, local)
+
+//var first = rhs.concat(rhs.alphabet, rhs.digit, special).repeat(1, infinity)
+//var local = rhs.concat(rhs.alphabet, rhs.digits, special,
+console.log(local)
