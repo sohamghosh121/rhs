17eaea3f51a8bc9f2c3ec4d68f08733b639d8238
Radahika <radhika.marvin@berkeley.edu>
2014-12-16 13:31:08 -0800

    put some spaces/newlines in

--- test.js
+++ test.js
@@ -5,21 +5,21 @@ if (typeof(module) !== 'undefined') {
 
 var re = new rhs("start");
 var func0 = re.compose(rhs.alter)(rhs.alter)(rhs.repeat)();
-console.log(func0("abc", "def", [4,8]).toString());
+console.log('\n', func0("abc", "def", [4,8]).toString());
 var nested = rhs.compose(func0)(rhs.concat)(rhs.repeat)(rhs.concat)(rhs.single)(rhs.concat)();
-console.log(nested(["abc", "def", [4, 8]], "end", 10000, "Please", [], ["yeah!"]).toString());
+console.log('\n', nested(["abc", "def", [4, 8]], "end", 10000, "Please", [], ["yeah!"]).toString());
 //(((abc)|(def)){4,8}end){10000}Please/
 
 var re = new rhs("start");
 var func1 = re.compose(rhs.alter)(rhs.repeat)();
 var func2 = re.compose(func1)(rhs.alter)(rhs.repeat)(rhs.concat)();
-console.log(func2(["friend group", [4, 8]], "[^Soham]", 10000, " Dear Bodik, please be merciful on our grades").toString());
+console.log('\n',func2(["friend group", [4, 8]], "[^Soham]", 10000, " Dear Bodik, please be merciful on our grades").toString());
 // ((((start)|(friend group)){4,8})|([^Soham])){10000} Dear Bodik, please be merciful on our grades
 
 var re = new rhs("Friends ");
 var func1 = rhs.compose(rhs.alter)(rhs.repeat)();
 var func2 = re.compose(rhs.concat)();
 var nested = re.compose(func1)(func2)();
-console.log(nested([["HongJun", "Soham "], 100000], "wee", "HongJun").toString());
+console.log('\n', nested([["HongJun", "Soham "], 100000], "wee", "HongJun").toString());
 
 //returns 'Friends HongJun'
